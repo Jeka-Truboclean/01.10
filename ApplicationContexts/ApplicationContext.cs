9c32603c5e9b6892b94ad5f8ac27b3e3bae9a045
Eugen Karaman <karamanjekakol@gmail.com>
2024-10-31 14:09:52 +0200

    Add project files.

---
+++ ApplicationContexts/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using _01._10.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._10.ApplicationContexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectModels;Database=RegDb;Trusted_Connection=True;");
+
+        }
+    }
+}
